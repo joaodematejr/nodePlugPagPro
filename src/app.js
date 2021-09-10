e987126f61dd5a350a9714fef2215520db48a582
joaodematejr <joaodematejr@gmail.com>
2021-09-10 17:26:38 -0300

    Atualização

--- src/app.js
+++ src/app.js
@@ -1,23 +1,38 @@
 const path = require('path')
+const readline = require('readline')
 
-var readline = require('readline')
-
-var StructType = require('ref-struct-napi')
-var ArrayType = require('ref-array-napi')
-
+const ref = require('ref-napi')
+const ArrayType = require('ref-array-napi')
+const Struct = require('ref-struct-di')(ref)
 const ffi = require('ffi-napi')
+
+var PagSeguro
+
+var TransactionResult = Struct({
+  rawBuffer: ArrayType('char *', 65543),
+  message: ArrayType('char *', 1024),
+  transactionCode: ArrayType('char *', 33),
+  date: ArrayType('char *', 11),
+  time: ArrayType('char *', 9),
+  hostNsu: ArrayType('char *', 13),
+  cardBrand: ArrayType('char *', 31),
+  bin: ArrayType('char *', 7),
+  holder: ArrayType('char *', 5),
+  userReference: ArrayType('char *', 11),
+  terminalSerialNumber: ArrayType('char *', 66),
+})
 
 function loadLibraries() {
   const dllPagSeguro = path.resolve(__dirname, '..', 'bin', 'PPPagSeguro.dll')
   const dllBTSerial = path.resolve(__dirname, '..', 'bin', 'BTSerial.dll')
-  const dllPlugPag = path.resolve(__dirname, '..', 'bin', 'PlugPag.dll')
+  //const dllPlugPag = path.resolve(__dirname, '..', 'bin', 'PlugPag.dll')
+  ffi.Library(dllBTSerial)
   return dllPagSeguro
 }
 
 const bluetooth_port = 'COM3'
 const app_name = 'PlugPagNode'
 const app_version = '1.0.0'
-const encoding = 'UTF-8'
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -36,75 +51,50 @@ function renderErr(codResponse) {
   }
 }
 
-async function initPlugPag(pagSeguroLib) {
-  console.log('Definindo nome e versão da aplicação... ')
-  let pgVersion = ffi.Library(pagSeguroLib, {
-    SetVersionName: ['int', ['string', 'string']],
-  })
-  let response = await pgVersion.SetVersionName(app_name, app_version)
-  renderErr(response)
-}
-
-async function initPlugPagBluetooth(pagSeguroLib) {
-  console.log('Configurando conexao bluetooth... ')
-  let pgBluetooth = ffi.Library(pagSeguroLib, {
-    InitBTConnection: ['int', ['string']],
-  })
-  let response = await pgBluetooth.InitBTConnection(bluetooth_port)
-  renderErr(response)
-}
-
-//VERSÃO
-function getVersion(dllPagSeguro) {
-  console.log('Recuperando Versão... ')
-  let pg = ffi.Library(dllPagSeguro, {
-    GetVersionLib: ['int', []],
-  })
-  return pg.GetVersionLib()
-}
-
 //PAGAMENTO
-async function handlePayment(dllPagSeguro) {
-  var transactionResult = StructType({
-    rawBuffer: ArrayType('char', 65543),
-    message: ArrayType('char', 1024),
-    transactionCode: ArrayType('char', 33),
-    date: ArrayType('char', 11),
-    time: ArrayType('char', 9),
-    hostNsu: ArrayType('char', 13),
-    cardBrand: ArrayType('char', 31),
-    bin: ArrayType('char', 7),
-    holder: ArrayType('char', 5),
-    userReference: ArrayType('char', 11),
-    terminalSerialNumber: ArrayType('char', 66),
-  })
-
+function handlePayment() {
   console.log('Aguardando pagamento...')
-  let pagSeguro = ffi.Library(dllPagSeguro, {
-    SimplePaymentTransaction: [
-      'int',
-      ['int', 'int', 'int', 'string', 'string', transactionResult],
-    ],
-  })
+}
+
+//ULTIMO PAGAMENTO
+function handleQueryLastTransaction() {
+  console.log('Consultando ultima transacao...')
+
+  TransactionResult.defineProperty('transactionResult', 
+  ArrayType(ref.refType(TransactionResult)))
+
+  const CPtr = ref.refType(TransactionResult);
+
+  let response = PagSeguro.GetLastApprovedTransactionStatus(TransactionResult)
+  
+
   try {
-    let response = await pagSeguro.SimplePaymentTransaction(
-      1,
-      1,
-      1,
-      '100',
-      'UserRef',
-      transactionResult,
-    )
-    console.log('62', response)
+    if (response === 0) {
+      console.log('response', response)
+      console.log('transactionResultPtr', struct_sockaddr_in)
+    } else {
+      renderErr(response)
+    }
   } catch (error) {
     console.log('error', error)
   }
 }
 
 function main() {
-  pp = loadLibraries()
-  initPlugPag(pp)
-  //initPlugPagBluetooth(pp)
+  dllPagSeguro = loadLibraries()
+  PagSeguro = ffi.Library(dllPagSeguro, {
+    GetLastApprovedTransactionStatus: ['int', [TransactionResult]],
+    GetVersionLib: ['int', []],
+    SetVersionName: ['int', ['string', 'string']],
+    InitBTConnection: ['int', ['string']],
+    SimplePaymentTransaction: ['int', ['int', 'int', 'int', 'string', 'string', TransactionResult]],
+    CancelTransaction: ['int', [TransactionResult]], 
+    UnloadDriverConnection: ['void', ['void']],
+  })
+  console.log('\n')
+  console.log('Definindo nome e versão da aplicação... ')
+  PagSeguro.SetVersionName(app_name, app_version)
+  PagSeguro.InitBTConnection(bluetooth_port)
   console.log('\n\n*** Pressione Ctrl+C para finalizar a aplicação ***')
   var leitor = function () {
     rl.question(
@@ -112,20 +102,21 @@ function main() {
       function (comando) {
         switch (comando) {
           case '1':
-            handlePayment(pp)
+            handlePayment()
             break
           case '2':
             console.log('222222222222222')
             break
           case '3':
-            console.log('333333333333333')
+            handleQueryLastTransaction()
             break
           case '4':
-            let responseVersion = getVersion(pp)
+            //VERSÃO
+            console.log('Recuperando Versão...')
+            let responseVersion = PagSeguro.GetVersionLib()
             console.log(`Versão ${responseVersion}`)
             break
           default:
-            console.log('!!! Opção invalida !!!')
             break
         }
         leitor()
